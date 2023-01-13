346e9bbab5edd75b9e6241e570a7ce82830ea31a
debb88 <meilianadebora@gmail.com>
2023-01-13 21:20:18 +0700

    main

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer>
+    <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <img src="img/logo.png" alt="logo" width="300px" height="300px" />
+            <div class="row justify-content-md-center">
+              <div class="col-md-auto">
+                <a href="https://www.facebook.com/">
+                  <i class="fa-brands fa-facebook-f icons"></i>
+                </a>
+              </div>
+              <div class="col-md-auto">
+                <a href="https://www.youtube.com/">
+                  <i class="fa-brands fa-youtube icons"></i>
+                </a>
+              </div>
+              <div class="col-md-auto">
+                <a href="https://www.discord.com/">
+                  <i class="fa-brands fa-discord icons"></i>
+                </a>
+              </div>
+              <div class="col-md-auto">
+                <a href="https://www.twitter.com/">
+                  <i class="fa-brands fa-twitter icons"></i>
+                </a>
+              </div>
+              <div class="col-md-auto">
+                <a href="https://www.instagram.com/">
+                  <i class="fa-brands fa-instagram icons"></i>
+                </a>
+              </div>
+            </div>
+            <div class="col order-1">
+              <li><a href="">Help Center</a></li>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="contact.html">Contact Us</a></li>
+              <li>
+                &copy; 25.10.2022 | Kelompok 3 | Debora | Westlee | Felixcia |
+                Wilson | Rahel
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ navbar.php
@@ -0,0 +1,91 @@
+<header>
+      <nav
+        class="navbar navbar-expand-lg bg-body-tertiary fixed-top"
+        data-bs-theme="dark"
+        style="background-color: black"
+      >
+        <div class="container-fluid">
+          <a href="home.html" class="navbar-brand"
+            ><img src="img/logo.png" alt="logo" width="30" height="24"
+          /></a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="home.php"
+                  >Home</a
+                >
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Genre
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Anime</a></li>
+                  <li>
+                    <a class="dropdown-item" href="#">Children & Family</a>
+                  </li>
+                  <li><a class="dropdown-item" href="#">Comedies</a></li>
+                  <li><a class="dropdown-item" href="#">Drama</a></li>
+                  <li><a class="dropdown-item" href="#">Romance</a></li>
+                  <li><a class="dropdown-item" href="#">Fantasy</a></li>
+                  <li><a class="dropdown-item" href="#">Horror</a></li>
+                  <li><a class="dropdown-item" href="#">Thriller</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="movies.php"
+                  >Movies</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="series.php"
+                  >Series</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="about.php"
+                  >About</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="contact.php"
+                  >Contact</a
+                >
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Searching.."
+                aria-label="Search"
+              />
+              <button class="btn btn btn-outline-info" type="submit">
+                Search
+              </button>
+            </form>
+            <li style="list-style: none; margin-left: 20px; margin-right: 10px;">
+              <a style="color: rgb(78, 208, 248);" href="#"><i class="fa-solid fa-user"></i></a>
+            </li>
+          </div>
+        </div>
+      </nav>
+    </header>
